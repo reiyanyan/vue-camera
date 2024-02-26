5a1db1af378a7c8311180f840c04f96dbe7a92c5
reiyanyan <hariyantoriyan.hr@gmail.com>
2024-02-26 18:07:19 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,114 @@
+# Vue Camera
+
+Customizable camera library for Vue 3, within asking permission to user.
+
+## Features
+
+- Asking Permission to user
+- Flip camera rear/front
+- Result blob/b64
+- Mirror Camera
+
+## Installation
+
+```
+npm i @reiyanyan/vue-camera
+```
+
+## Usage
+
+To use it in your Vue app
+
+First, import in your custom component for proxy camera
+
+```
+import VxCamera from "@reiyanyan/vue-camera"
+```
+
+Second, create ref for canvas and video:
+
+- video for camera places
+- canvas for result after camera taken
+
+```
+const videoRef = ref<HTMLVideoElement>();
+const canvasRef = ref<HTMLCanvasElement>();
+```
+
+Third, create variable to memoize your camera instance
+
+```
+const cameraRef = ref();
+```
+
+Fourth, create an function for starting camera
+
+example:
+
+```
+const permissionDenied = ref(false);
+const isCameraOpen = ref(false);
+
+async function startCamera () {
+
+    const checkPermission = setInterval(() => {
+        if (isCameraOpen.value) {
+        clearInterval(checkPermission);
+        } else {
+        permissionDenied.value = true;
+        clearInterval(checkPermission);
+        }
+    }, 1000);
+
+    cameraRef.value = await new VxCamera(videoRef.value!, canvasRef.value!)
+            .setConstraint({
+                video: {
+                    facingMode: "environment",
+                    height: {
+                    min: 480,
+                    max: 480,
+                    ideal: 480,
+                    },
+                    width: {
+                    min: 480,
+                    max: 480,
+                    ideal: 480,
+                    },
+                },
+                audio: false,
+            })
+            .requestPermission()
+            .then((camera) => camera)
+            .catch((err) => {
+                console.error(err);
+            });
+
+    cameraRef.value?.start().finally(() => (isCameraOpen.value = true));
+}
+```
+
+- reference of constraint [MDN MediaTrackConstraints](https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackConstraints)
+
+Fifth, taking a picture and save as blob/b64
+
+```
+const result = await cameraRef.value?.snapAsBlob().then((data: any) => data)
+// or
+const result = await cameraRef.value?.snapAsBase64().then((data: any) => data)
+
+snapAsBase64 or snapAsBlob
+```
+
+That was example using the library to your project.
+
+## Contributing
+
+Contributions are always welcome! Feel free to fork the [repository](https://reiyanyan/vue-camera) and submit pull requests.
+
+## Issues
+
+If you find any bug or have a feature request, please file an issue on [Github](https://reiyanyan/vue-camera).
+
+---
+
+Happy Developing!
---
+++ index.ts
@@ -0,0 +1,208 @@
+/* -------------------------------------------------------------------------- */
+/*                                 Simple Docs                                */
+/* -------------------------------------------------------------------------- */
+/**
+ *
+ * function start with prefix 'set', used before requestPermission
+ *
+ */
+
+type FacingModeProps = "environment" | "user";
+
+type MediaConstraintsProps = {
+  video: VideoConstraintsProps;
+  audio: boolean;
+};
+
+type VideoConstraintsProps = {
+  facingMode: FacingModeProps;
+  width?: ResolutionRequestProps;
+  height?: ResolutionRequestProps;
+};
+
+type ResolutionRequestProps = {
+  min?: number;
+  max?: number;
+  ideal?: number;
+};
+
+export default class ReiCamera {
+  video!: HTMLVideoElement;
+  canvas!: HTMLCanvasElement;
+  customConstraint: MediaConstraintsProps = {} as MediaConstraintsProps;
+  devices: Array<unknown> = [];
+  stream: MediaStream = {} as MediaStream;
+
+  public constructor(video: HTMLVideoElement, canvas: HTMLCanvasElement) {
+    this.video = video;
+    this.canvas = canvas;
+  }
+
+  /* --------------------------- Default Constraint --------------------------- */
+  DEFAULT_CONSTRAINT: MediaConstraintsProps = {
+    video: {
+      facingMode: "user",
+    },
+    audio: false,
+  };
+
+  /* -------------------------------------------------------------------------- */
+  /*                                Internal usage                              */
+  /* -------------------------------------------------------------------------- */
+
+  private constraint(): MediaConstraintsProps {
+    if (Object.keys(this.customConstraint).length === 0) {
+      return this.DEFAULT_CONSTRAINT;
+    } else {
+      return this.customConstraint;
+    }
+  }
+
+  private getDevices(): Promise<Array<unknown>> {
+    return new Promise((resolve, reject) => {
+      if (this.devices.length > 0) {
+        resolve(this.devices);
+        return;
+      }
+      try {
+        navigator.mediaDevices.enumerateDevices().then((devices) => {
+          devices.forEach((device) => {
+            if (device.kind && device.kind.toLocaleLowerCase() === "videoinput")
+              this.devices.push(device);
+          });
+        });
+        resolve(this.devices);
+      } catch (error) {
+        console.error("GetDevices", error);
+        reject(error);
+      }
+    });
+  }
+
+  private stop() {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    if (!this.video && !(this.video as any)?.srcObject) return;
+    if (this.video.srcObject instanceof MediaStream) {
+      const tracks = this.video.srcObject.getTracks();
+      tracks.forEach((track) => track.stop());
+    } else {
+      const tracks = this.stream.getTracks();
+      tracks.forEach((track) => track.stop());
+    }
+    this.video.srcObject = null;
+  }
+
+  private snap(): HTMLCanvasElement {
+    this.canvas.width = this.video.videoWidth;
+    this.canvas.height = this.video.videoHeight;
+    const context = this.canvas.getContext("2d");
+    // if (context) {
+    context?.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    context?.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
+    // }
+    return this.canvas;
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                               Provide public                               */
+  /* -------------------------------------------------------------------------- */
+
+  /* ------------------- Save this to ref in ur vue setup ------------------- */
+  public requestPermission(): Promise<ReiCamera> {
+    return new Promise<ReiCamera>((resolve, reject) => {
+      try {
+        navigator.mediaDevices.getUserMedia(this.constraint()).then(() => {
+          resolve(this);
+        });
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  /* ---------------------------- Start the camera ---------------------------- */
+  public start(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      try {
+        this.getDevices().then(() => {
+          navigator.mediaDevices.getUserMedia(this.constraint()).then((stream) => {
+            this.video.srcObject = stream;
+            this.stream = stream;
+          });
+        });
+        resolve();
+      } catch (error) {
+        console.error("StartAsync", error);
+        reject(error);
+      }
+    });
+  }
+
+  /* ------------------------ Horizontal mirror or flip ----------------------- */
+  public mirror(isMirror: boolean): ReiCamera {
+    this.video.style.transform = `scaleX(${isMirror ? "-1" : "1"})`;
+    return this;
+  }
+
+  /* ----------------------- Flip front and rear camera ----------------------- */
+  public flipCamera(mode?: FacingModeProps): ReiCamera {
+    if (mode) {
+      this.setConstraint({
+        video: {
+          ...this.constraint().video,
+          facingMode: mode,
+        },
+        audio: this.constraint().audio,
+      });
+    } else {
+      this.setConstraint({
+        video: {
+          ...this.constraint().video,
+          facingMode: this.constraint().video.facingMode == "user" ? "environment" : "user",
+        },
+        audio: this.constraint().audio,
+      });
+    }
+
+    this.stop();
+
+    try {
+      this.start();
+    } catch (err) {
+      console.error("FlipCamera", err);
+    }
+
+    return this;
+  }
+
+  public snapAsBlob(mime = "image/png") {
+    return new Promise((resolve, reject) => {
+      try {
+        this.snap().toBlob((blob) => resolve(blob), mime, 1);
+      } catch (err) {
+        console.error("SnapAsBlob", err);
+        reject(err);
+      }
+    });
+  }
+
+  public snapAsBase64(mime = "image/png") {
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(this.snap().toDataURL(mime));
+      } catch (err) {
+        console.error("Snap as b64", err);
+        reject(err);
+      }
+    });
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                           'set' prefix function                            */
+  /* -------------------------------------------------------------------------- */
+
+  public setConstraint(constraint: MediaConstraintsProps): ReiCamera {
+    this.customConstraint = constraint;
+    return this;
+  }
+}
